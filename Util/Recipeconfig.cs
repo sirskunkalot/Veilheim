9d46c86c6ba887bf25c0e822d88abd22b12ec609
shuwa <dogmatus@hotmail.com>
2021-03-05 06:49:40 +0100

    Added Shared Project Util -> moved from Veilheim. Updated Util Classes. Added VeilheimItems Project -> WIP

---
+++ Util/Recipeconfig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Veilheim.Util
+{
+   
+
+    [Serializable]
+    public class RecipeRequirementConfig
+    {
+        public string item;
+        public int amount;
+    }
+
+    [Serializable]
+    public class RecipeConfig
+    {
+        public string name;
+        public string item;
+        public int amount;
+        public string craftingStation;
+        public int minStationLevel;
+        public bool enabled;
+        public string repairStation;
+        public List<RecipeRequirementConfig> resources = new List<RecipeRequirementConfig>();
+    }
+
+    [Serializable]
+    public class RecipesConfig
+    {
+        public List<RecipeConfig> recipes = new List<RecipeConfig>();
+    }
+}
