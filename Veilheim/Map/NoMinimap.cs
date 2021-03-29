fc3fc55badbd6f753618852bf0314f88c70d0c5e
Algorithman <algorithman.chris@googlemail.com>
2021-03-29 20:08:10 +0200

    fix for no minimap mode

--- Veilheim/Map/NoMinimap.cs
+++ Veilheim/Map/NoMinimap.cs
@@ -4,6 +4,7 @@
 // File:    NoMinimap.cs
 // Project: Veilheim
 
+using UnityEngine;
 using Veilheim.Configurations;
 using Veilheim.PatchEvents;
 
@@ -11,15 +12,35 @@ namespace Veilheim.Map
 {
     public class NoMinimap : IPatchEventConsumer
     {
+        private static bool hideMinimap = false;
 
-        [PatchEvent(typeof(Minimap), nameof(Minimap.SetMapMode), PatchEventType.Prefix)]
-        public static void DontShowMinimap_Patch(Minimap instance, ref Minimap.MapMode mode)
+        [PatchEvent(typeof(Minimap), nameof(Minimap.SetMapMode), PatchEventType.BlockingPrefix)]
+        public static void DontShowMinimap_Patch(Minimap instance, ref Minimap.MapMode mode, ref bool cancel)
         {
             if (Configuration.Current.Map.showNoMinimap)
             {
-                if (mode == Minimap.MapMode.Small)
+                if ((Chat.instance == null || !Chat.instance.HasFocus()) && !global::Console.IsVisible() && !TextInput.IsVisible() && !Menu.IsVisible() && !InventoryGui.IsVisible() && !Minimap.InTextInput())
                 {
-                    mode = Minimap.MapMode.None;
+                    if (ZInput.GetButtonDown("Map") || ZInput.GetButtonDown("JoyMap") || (instance.m_mode == Minimap.MapMode.Large &&
+                                                                                          (Input.GetKeyDown(KeyCode.Escape) || ZInput.GetButtonDown("JoyButtonB"))))
+                    {
+                        if (mode == Minimap.MapMode.Large)
+                        {
+                            return;
+                        }
+                        
+                        if ((mode == Minimap.MapMode.Small) && (instance.m_mode == Minimap.MapMode.Large))
+                        {
+                            mode = Minimap.MapMode.None;
+                        }
+                    }
+                    else
+                    {
+                        if ((mode == Minimap.MapMode.Small) && (instance.m_mode == Minimap.MapMode.None))
+                        {
+                            mode = Minimap.MapMode.None;
+                        }
+                    }
                 }
             }
         }
